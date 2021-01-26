95ce69a6cfe975c69b170cf1d85c8ff2c3f9169f
Froris <gamefoxmail@gmail.com>
2021-01-26 16:32:41 +0200

    cubes render rework & optimization

--- src/components/fieldContainer/cubesRender/CubesRender.js
+++ src/components/fieldContainer/cubesRender/CubesRender.js
@@ -1,59 +1,26 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
 import "./cube.scss";
-import { AppContext } from "../../../context/AppContext";
-import generateCubes from "../utils/generateCubes";
 
-const CubesRender = ({ newCubes, setPoints, setNewCubes }) => {
-  const [state] = useContext(AppContext);
+const CubesRender = ({ newCubes }) => {
   const [cubesList, updateCubesList] = useState(newCubes);
 
-  // Если в массиве отображаемых кубов остался один:
-  // Вызываем генератор и обновляем список отображаемых кубов.
-  // Если кубов несколько - фильтруем массив и убираем тот, на который навели курсор.
-  const cubesUpdater = (id) => {
-    if (cubesList.length && id) {
-      if (cubesList.length === 1) {
-        setNewCubes(generateCubes(state.cubesData));
-        updateCubesList(newCubes);
-      } else if (cubesList.length > 1) {
-        const newArr = cubesList.filter((cube) => {
-          return cube.id !== id;
-        });
-        updateCubesList(newArr);
-      }
-    }
-  };
+  useEffect(() => {
+    updateCubesList(newCubes);
+  }, [newCubes]);
 
-  const onMouseOverHandler = (points, id) => {
-    if (!state.isGamePaused) {
-      setPoints(points);
-      cubesUpdater(id);
-    }
-
-    return;
-  };
-
+  // Выводим кубы из списка
   return cubesList.map((cube, index) => {
     const cubeClass = classNames({
       cube: true,
       regular: cube.type === "regular",
       rare: cube.type === "rare",
-      gold: cube.type === "ssr",
+      ssr: cube.type === "ssr",
     });
 
-    return (
-      <div
-        key={index}
-        className={cubeClass}
-        style={{ ...cube.position }}
-        onClick={() => {
-          onMouseOverHandler(cube.points, cube.id);
-        }}
-      ></div>
-    );
+    return <div key={index} className={cubeClass} style={{ ...cube.position }} id={cube.id}></div>;
   });
 };
 
@@ -61,6 +28,4 @@ export default CubesRender;
 
 CubesRender.propTypes = {
   newCubes: PropTypes.array,
-  setPoints: PropTypes.func,
-  setNewCubes: PropTypes.func,
 };
--- src/components/fieldContainer/field/Field.js
+++ src/components/fieldContainer/field/Field.js
@@ -10,8 +10,40 @@ const Field = () => {
   const [state, dispatch] = useContext(AppContext);
   const [cubes, setNewCubes] = useState([]);
 
-  const setPoints = (points) => {
-    dispatch({ type: SET_POINTS, payload: points });
+  const setPoints = (type) => {
+    const tappedCube = cubes.find((cube) => cube.type === type);
+    dispatch({ type: SET_POINTS, payload: tappedCube.points });
+  };
+
+  // Если в массиве отображаемых кубов остался один:
+  // Вызываем генератор и обновляем список отображаемых кубов.
+  // Если кубов несколько - фильтруем массив и убираем тот, на который навели курсор.
+  const cubesListUpdater = (id) => {
+    const cubeId = parseInt(id, 10);
+    if (cubes.length && cubeId) {
+      if (cubes.length === 1) {
+        setNewCubes(generateCubes(state.cubesData));
+      } else if (cubes.length > 1) {
+        const newArr = cubes.filter((cube) => {
+          return cube.id !== cubeId;
+        });
+        setNewCubes(newArr);
+      }
+    }
+  };
+
+  const onMouseClickHandler = (e) => {
+    const cubeClassName = e.target.className;
+    if (cubeClassName.includes("cube")) {
+      const cubeType = cubeClassName.split(" ")[1];
+      const cubeId = e.target.id;
+
+      if (!state.isGamePaused) {
+        setPoints(cubeType);
+        cubesListUpdater(cubeId);
+      }
+    }
+    return;
   };
 
   // Создаём начальный список кубов при первом рендере
@@ -20,8 +52,8 @@ const Field = () => {
   }, []);
 
   return (
-    <div className="field-container">
-      {state.isGameStarted && <CubesRender newCubes={cubes} setPoints={setPoints} setNewCubes={setNewCubes} />}
+    <div className="field-container" onClick={onMouseClickHandler}>
+      {state.isGameStarted && <CubesRender newCubes={cubes} />}
     </div>
   );
 };
